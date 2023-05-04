ff4fd4be028addde281d6bcc4336bd0ffaf0c35c
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-05-04 19:17:04 +0200

    implementazione slider-item dinamico finita

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
 // dichiarazione di un array per lo storage delle immagini
 let arrayOfImages = [];
 
-// Push the URLs to three images to arrayOfImages
-arrayOfImages.push('../img/01.webp');
-arrayOfImages.push('../img/02.webp');
-arrayOfImages.push('../img/03.webp');
-arrayOfImages.push('../img/04.webp');
-arrayOfImages.push('../img/05.webp');
+let activeItem = 0;
 
-// Output arrayOfImages to the console
-console.log(arrayOfImages);
+// Mettiamo il path delle immagini nell'arrayOfImages
+arrayOfImages.push('./img/01.webp');
+arrayOfImages.push('./img/02.webp');
+arrayOfImages.push('./img/03.webp');
+arrayOfImages.push('./img/04.webp');
+arrayOfImages.push('./img/05.webp');
+
+//dichiarazione costante per lo slider container
+const  sliderContainer = document.getElementById('slider-container');
+
+//ciclo per creare dinamicamente gli slider items
+for(let i = 0; i <= arrayOfImages.length - 1; i++){
+
+    //creazione div per slider item
+    let element = document.createElement('div');
+
+    //aggiunta classe per slider item
+    element.classList.add("slider-item");
+
+    //append slider item
+    sliderContainer.append(element);
+
+    if(i === 0 && activeItem === 0){
+        element.classList.add("active");
+    }
+
+    //inserimento immagini nello slider item
+    element.innerHTML = `<img src="${arrayOfImages[i]}" alt="">`;
+
+
+
+}
+
+
