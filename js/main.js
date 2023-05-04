b3ba7a8be4244954afb7ae49a409f361d3b1549e
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-05-04 17:37:33 +0200

    markup completato e stilato

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// dichiarazione di un array per lo storage delle immagini
+let arrayOfImages = [];
+
+// Push the URLs to three images to arrayOfImages
+arrayOfImages.push('../img/01.webp');
+arrayOfImages.push('../img/02.webp');
+arrayOfImages.push('../img/03.webp');
+arrayOfImages.push('../img/04.webp');
+arrayOfImages.push('../img/05.webp');
+
+// Output arrayOfImages to the console
+console.log(arrayOfImages);
