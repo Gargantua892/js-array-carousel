e71fa029a21098268a4060be917df4a8dbaf434b
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-05-05 16:31:44 +0200

    esercizio bonus refactoring

--- bonus/js/main-bonus.js
+++ bonus/js/main-bonus.js
@@ -1,11 +1,14 @@
 // dichiarazione di un array per lo storage delle immagini
 let arrayOfImages = [];
-
 let activeItem = 0;
 
 //definizione costanti per frecce di navigazione
 const topArrow = document.getElementById("top-arrow");
 const bottomArrow = document.getElementById("bottom-arrow");
+const thumbContainer = document.getElementById("thumbnail-container");
+console.log(thumbContainer);
+
+
 
 
 // Mettiamo il path delle immagini nell'arrayOfImages
@@ -23,59 +26,64 @@ for(let i = 0; i < arrayOfImages.length; i++){
 
     //creazione div per slider item
     let element = document.createElement('div');
+    let thumb = document.createElement('div');
 
     //aggiunta classe per slider item
     element.classList.add("slider-item");
+
+        //aggiunta classe per slider thumb
+        thumb.classList.add("slider-thumb", "overlay");
+
+
+        //append slider thumb
+        thumbContainer.append(thumb);
+
 
     //append slider item
     sliderContainer.append(element);
 
     //inserimento immagini nello slider item
-    element.innerHTML = `<img src="${arrayOfImages[i]}" alt="">`;   
+    element.innerHTML = `<img src="${arrayOfImages[i]}" alt="">`;
+    thumb.innerHTML = `<img src="${arrayOfImages[i]}" alt="">`;
 
 }
 
+
 //dichiaro la variabile items per selezionare la classe di tutti gli elementi slider-items
 let items = document.querySelectorAll(".slider-item");
+let thumbItems = document.querySelectorAll(".slider-thumb");
 
 //attribuisco la classe active all'indice 0 dell'array
 items[activeItem].classList.add("active");
-console.log(activeItem);
+thumbItems[activeItem].classList.remove("overlay");
+
+// //aggiunta event listner freccia di navigazione bottom
+bottomArrow.addEventListener("click", 
+function(){
+    items[activeItem].classList.remove("active");
+    thumbItems[activeItem].classList.toggle("overlay");
+    activeItem++;
+    items[activeItem].classList.add("active");
+    thumbItems[activeItem].classList.toggle("overlay", "");
+
+    if(items > arrayOfImages.length){
+        items[activeItem].classList.remove("active");
+        activeItem = 0;
+    }
+
+    
+
+});
 
 // //aggiunta event listner freccia di navigazione top
 topArrow.addEventListener("click", 
 function(){
     items[activeItem].classList.remove("active");
-    activeItem++;
-    console.log(activeItem);
-    items[activeItem].classList.add("active");
-
-    //nascondo la freccia se ultimo elemento dell'array
-    if (activeItem === items.length -1){
-        topArrow.classList.toggle("hidden");
-    }
-    else{
-        document.getElementById("bottom-arrow").classList.remove("hidden");
-    }
-});
-
-//Nacondo la freccia in basso di default
-bottomArrow.classList.add("hidden");
-// //aggiunta event listner freccia di navigazione bottom
-bottomArrow.addEventListener("click", 
-function(){
-    items[activeItem].classList.remove("active");
+    thumbItems[activeItem].classList.toggle("overlay");
     activeItem--;
-    console.log(activeItem);
-    items[activeItem].classList.add("active");
 
-    //nascondo la freccia se ultimo elemento dell'array
-    if (activeItem === 0){
-        bottomArrow.classList.add("hidden");
-    }
-    else{
-        topArrow.classList.remove("hidden");
-    }
+    items[activeItem].classList.add("active");
+    thumbItems[activeItem].classList.toggle("overlay", "");
 });
 
 
